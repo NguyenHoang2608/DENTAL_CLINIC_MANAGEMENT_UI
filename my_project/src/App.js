69e1082829bba1967735bd0ba52084a34f62027a
PhungNguyenHoang <89393736+PhungNguyenHoang@users.noreply.github.com>
2022-10-03 21:45:46 +0700

    gdfcs

--- my_project/src/App.js
+++ my_project/src/App.js
@@ -1,14 +1,15 @@
 import Navbarr from "./components/navbar";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
+import Login from "./login_UI/login";
+import Homepage_ui from "./home_ui/homepage"
 function App() {
   return (
     <Router>
-    <Navbarr/>
+      <Navbarr/>
     <Routes>
-      {/* <Route path="/" exact element={<Home />}></Route> */}
-
-      
+       
+      <Route path="/Login" element={<Login/>} />
+      <Route path="/homepage" element={<Homepage_ui/>} />
     </Routes>
   </Router>
   );
